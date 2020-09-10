228ae94d4d6276230e9547ee0de40edd52e29a96
Anders Olme <anders.olme@gmail.com>
2020-09-10 05:23:40 +0200

    Small updates one step at a time

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-enum JType{
+enum JType {
     JString,
-	JNumber,
-	JArray,
-	JObject
+    JNumber,
+    JArray,
+    JObject,
 }
 
-struct JSONValue{
-    strValue String
+struct JSONValue {
+    jtype: JType,
+    strValue: String,
+    arr: Vec<JSONValue>,
 }
 
 fn main() {
