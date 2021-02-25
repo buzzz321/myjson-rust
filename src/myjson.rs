04a77aa8f2661039cb7b6230937bc21b93d56394
Anders Olme <anders.olme@gmail.com>
2021-02-25 20:33:57 +0100

    Small update moving around stuff

--- src/myjson.rs
+++ src/myjson.rs
@@ -13,19 +13,32 @@ pub struct JSONValue<'a> {
     arr: Vec<JSONValue<'a>>,
 }
 trait Parser {
+    //fn parse_qouted_string(&mut self) ->&str;
     fn consume_white_space(&mut self);
     fn consume(&mut self, token: &str)-> bool;
     fn get_data(&self) -> &str;
     fn is_digit(&self,val: &str)->bool;
 }
 
-#[derive(Debug)]
 pub struct ParserData<'a> {
     data: &'a str,
     curr_pos: usize,
 }
 
+
 impl <'a>Parser for ParserData<'a> {
+  /*   fn parse_qouted_string(&mut self) ->&str {
+        self.consume_white_space();
+
+        if self.data.chars().nth(self.curr_pos) != Some('\"'){
+            return "";
+        }
+        let mut end_found = false;
+        let mut start_pos = self.curr_pos;
+        let mut c1 = self.data.char_indices().peekable();
+        c1.skip(self.curr_pos);
+    }
+    */
     fn consume_white_space(&mut self) {
         for elem in self.data.chars().enumerate() {
             //println!("{} |{}|", self.curr_pos, elem.1);
@@ -35,6 +48,20 @@ impl <'a>Parser for ParserData<'a> {
             self.curr_pos += 1;
         }
     }
+
+    fn consume(&mut self, token: &str) -> bool {
+        if token.chars().nth(0).unwrap() != self.data.chars().nth(self.curr_pos).unwrap() {              
+            return false;
+        }
+        self.curr_pos += token.len();
+
+        if self.curr_pos> self.data.len() {
+            self.curr_pos = self.data.len() -1;
+        }
+
+        return true;
+    }
+
     fn get_data(&self) -> &str {
         //println!("{} ", self.curr_pos);
         &self.data[self.curr_pos..self.curr_pos + 1]
@@ -54,19 +81,6 @@ impl <'a>Parser for ParserData<'a> {
             }
         }
         false
-    }
-
-    fn consume(&mut self, token: &str) -> bool {
-        if token.is_char_boundary(0) != self.data.is_char_boundary(self.curr_pos) {
-            return false;
-        }
-        self.curr_pos += token.len();
-
-        if self.curr_pos> self.data.len() {
-            self.curr_pos = self.data.len() -1;
-        }
-
-        return true;
     }
 }
 
